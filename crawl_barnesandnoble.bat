fe51eff038d497b268f31909af081172dbfab08f
Diénert Vieira <dienertalencar@gmail.com>
2022-03-26 13:23:04 -0300

    Organizing url of B and N crawler

--- crawl_barnesandnoble.bat
+++ crawl_barnesandnoble.bat
@@ -1,3 +1,4 @@
 cd src
+@REM scrapy runspider crawler_barnesandnoble.py -O "../dataset/test_crawler_barnesandnoble- A Game of Thrones - A Song of Ice and Fire.csv"
 scrapy runspider crawler_barnesandnoble.py -O "../dataset/test_crawler_barnesandnoble.csv"
 cd ..
--- src/crawler_barnesandnoble.py
+++ src/crawler_barnesandnoble.py
@@ -10,8 +10,27 @@ class BarnesandnobleSpider(scrapy.Spider):
     limit = 100
     page = 0
     totalPages = None
-    url = 'https://api.bazaarvoice.com/data/batch.json?passkey=caC2Xb0kazery1Vgcza74qqETLsDbclQWr3kbWiGXSvjI&apiversion=5.5&displaycode=19386_1_0-en_us&resource.q1=reviews&filter.q1=productid:eq:9780316017930&filter.q1=contentlocale:eq:en_US&sort.q1=isfeatured:desc&stats.q1=reviews&filteredstats.q1=reviews&include.q1=authors&filter_reviews.q1=contentlocale:eq:en_US&filter_reviewcomments.q1=contentlocale:eq:en_US&filter_comments.q1=contentlocale:eq:en_US&limit.q1={}&offset.q1={}&callback=BV._internal.dataHandler0'
-    start_urls = [url.format(limit, offset)]
+    # productId = '9780316017930' # ISBN-13 of product - Outliers: The Story of Success
+    productId = '9780553381689' # ISBN-13 of product - A Game of Thrones (A Song of Ice and Fire #1)
+    url = 'https://api.bazaarvoice.com/data/batch.json\
+?passkey=caC2Xb0kazery1Vgcza74qqETLsDbclQWr3kbWiGXSvjI\
+&apiversion=5.5\
+&displaycode=19386_1_0-en_us\
+&resource.q1=reviews\
+&filter.q1=productid:eq:{}\
+&filter.q1=contentlocale:eq:en_US\
+&sort.q1=isfeatured:desc\
+&stats.q1=reviews\
+&filteredstats.q1=reviews\
+&include.q1=authors\
+&filter_reviews.q1=contentlocale:eq:en_US\
+&filter_reviewcomments.q1=contentlocale:eq:en_US\
+&filter_comments.q1=contentlocale:eq:en_US\
+&limit.q1={}\
+&offset.q1={}\
+&callback=BV._internal.dataHandler0'
+
+    start_urls = [url.format(productId, limit, offset)]
 
 
     def parse(self, response):
@@ -35,5 +54,7 @@ class BarnesandnobleSpider(scrapy.Spider):
         if self.page < self.totalPages:
             self.offset += self.limit            
             self.page += 1            
-            yield response.follow(self.url.format(self.limit, self.offset))
+            yield response.follow(self.url.format(self.productId,
+                                                  self.limit, 
+                                                  self.offset))
 
