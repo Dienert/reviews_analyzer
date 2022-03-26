4b6f8deb4a113f52b799a66d4ddc633b17e9b0ee
Diénert Vieira <dienertalencar@gmail.com>
2022-03-26 13:05:36 -0300

    Barnes and noble crawler with pagination working

--- crawl_barnesandnoble.bat
+++ crawl_barnesandnoble.bat
@@ -1,4 +1,3 @@
 cd src
-@REM scrapy runspider crawler_readbooks.py -o ../dataset/test_crawler_readbooks.csv -s LOG_ENABLED=False
-scrapy runspider crawler_barnesandnoble.py -o ../dataset/test_crawler_barnesandnoble.csv 
+scrapy runspider crawler_barnesandnoble.py -O "../dataset/test_crawler_barnesandnoble.csv"
 cd ..
--- src/crawler_barnesandnoble.py
+++ src/crawler_barnesandnoble.py
@@ -1,28 +1,28 @@
 import scrapy
 import json
+import math
 
 
 class BarnesandnobleSpider(scrapy.Spider):
     name = 'barnesandnoblespider'
-    start_urls = ['https://api.bazaarvoice.com/data/batch.json?passkey=caC2Xb0kazery1Vgcza74qqETLsDbclQWr3kbWiGXSvjI&apiversion=5.5&displaycode=19386_1_0-en_us&resource.q0=products&filter.q0=id:eq:9780316017930&stats.q0=reviews&filteredstats.q0=reviews&filter_reviews.q0=contentlocale:eq:en_US&filter_reviewcomments.q0=contentlocale:eq:en_US&resource.q1=reviews&filter.q1=isratingsonly:eq:false&filter.q1=productid:eq:9780316017930&filter.q1=contentlocale:eq:en_US&sort.q1=isfeatured:desc&stats.q1=reviews&filteredstats.q1=reviews&include.q1=authors,products,comments&filter_reviews.q1=contentlocale:eq:en_US&filter_reviewcomments.q1=contentlocale:eq:en_US&filter_comments.q1=contentlocale:eq:en_US&limit.q1=8&offset.q1=0&limit_comments.q1=3&callback=BV._internal.dataHandler0']
-    # start_urls = ['https://www.barnesandnoble.com/w/outliers-malcolm-gladwell/1100030024?ean=9780316017930']
-    page = 0 
+    totalResults = None
+    offset = 0
+    limit = 100
+    page = 0
+    totalPages = None
+    url = 'https://api.bazaarvoice.com/data/batch.json?passkey=caC2Xb0kazery1Vgcza74qqETLsDbclQWr3kbWiGXSvjI&apiversion=5.5&displaycode=19386_1_0-en_us&resource.q1=reviews&filter.q1=productid:eq:9780316017930&filter.q1=contentlocale:eq:en_US&sort.q1=isfeatured:desc&stats.q1=reviews&filteredstats.q1=reviews&include.q1=authors&filter_reviews.q1=contentlocale:eq:en_US&filter_reviewcomments.q1=contentlocale:eq:en_US&filter_comments.q1=contentlocale:eq:en_US&limit.q1={}&offset.q1={}&callback=BV._internal.dataHandler0'
+    start_urls = [url.format(limit, offset)]
 
 
     def parse(self, response):
         resp = response.text[26:-1]
-        # print(resp)
-        # print(type(resp))
         json_resp = json.loads(resp)
 
-        #check new data type
-        # print(type(json_resp))
-        # print(json_resp)
-            
-            
-        # stars_dict = {'it was amazing': 5, 'really liked it': 4, 'liked it': 3, 'it was ok': 2, 'did not like it': 1}
+        for _, batchedResult in json_resp['BatchedResults'].items():
+            if not self.totalResults:
+                self.totalResults = batchedResult['TotalResults']
+                self.totalPages = math.ceil(self.totalResults/self.limit)
 
-        for _, batchedResult in json_resp['BatchedResults'].items():#['q0']['Results']:
             for result in batchedResult['Results']:
 
                 yield {'reviewer': result.get('UserNickname', ''),
@@ -32,9 +32,8 @@ class BarnesandnobleSpider(scrapy.Spider):
                    'date': result.get('SubmissionTime', ''),
                 }
                 
-        # next_page_link = response.css('bv-content-pagination-buttons-item a::attr(href)').get()
-        # if next_page_link:
-        #     yield response.follow(next_page_link)
-
-            
+        if self.page < self.totalPages:
+            self.offset += self.limit            
+            self.page += 1            
+            yield response.follow(self.url.format(self.limit, self.offset))
 
