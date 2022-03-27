0acc3803ef1d34f870dee2a4982403f380fe50bc
Chrystinne <chrystinne@gmail.com>
2022-03-27 00:48:33 -0300

    Updating .bat to execute crawl audible

--- crawl_all.bat
+++ crawl_all.bat
@@ -3,4 +3,5 @@ scrapy runspider crawler_amazon.py -O ../dataset/amazon.csv
 scrapy runspider crawler_barnesandnoble.py -O ../dataset/barnesandnoble.csv
 scrapy runspider crawler_flipkart.py -O ../dataset/flipkart.csv 
 scrapy runspider crawler_goodreads.py -O ../dataset/goodreads.csv
+scrapy runspider crawler_audible.py -O ../dataset/audible.csv
 cd ..
